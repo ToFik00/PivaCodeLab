9572d7645a8deb6c8cb8c670e69e6585238f908c
ToFik <tofikpetya@gmail.com>
2025-03-15 14:16:34 +0300

    Initial commit

---
+++ src/main/java/org/piva/codelab/PivaCodeLabApplication.java
@@ -0,0 +1,13 @@
+package org.piva.codelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PivaCodeLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PivaCodeLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/piva/codelab/PivaCodeLabApplicationTests.java
@@ -0,0 +1,13 @@
+package org.piva.codelab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PivaCodeLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
